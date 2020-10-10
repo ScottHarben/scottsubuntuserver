59b96f40ebc128220ec019b74dd9f47a82e03732
Scott <scottharben@gmail.com>
2020-10-09 20:53:41 -0400

    db test 1

--- scottsubuntuserver.js
+++ scottsubuntuserver.js
@@ -1,14 +1,38 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const port = 9000
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 9000;
+const mysql = require('mysql');
 
-app.use(cors())
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'superuser',
+    password: 'ff44Fourpm',
+    database: 'Golf'
+});
+
+// var connection = mysql.createConnection({
+//     host: '10.0.0.35',
+//     port: '3306',
+//     user: 'remoteuser',
+//     password: 'ff44Fourpm',
+//     database: 'Golf'
+// });
+
+app.use(cors());
 
 app.get('/api', (req, res) => {
-    res.send('API')
-})
+    connection.connect();
+    connection.query('CALL spGetTopRankedPlayer()', (error, results, fields) => {
+        if (error) {
+            return console.error(error.message);
+        } else {
+            res.send(results[0])
+        }
+    });
+});
 
 app.listen(port, () => {
-    console.log('server listening at port:' + port)
-})
+    console.log('server listening at port:' + port);
+});
