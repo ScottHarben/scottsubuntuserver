03886cf502e61fdaaf157265d864058245ed01cc
Scott <scottharben@gmail.com>
2020-12-15 20:34:21 -0500

    remove password

--- scottsubuntuserver.js
+++ scottsubuntuserver.js
@@ -4,21 +4,21 @@ const cors = require("cors");
 const port = 9000;
 const mysql = require("mysql");
 
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: "3306",
+//   user: "superuser",
+//   password: "",
+//   database: "Golf",
+// });
+
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "10.0.0.35",
   port: "3306",
-  user: "superuser",
+  user: "remoteuser",
   password: "ff44Fourpm",
   database: "Golf",
 });
-
-// var connection = mysql.createConnection({
-//   host: "10.0.0.35",
-//   port: "3306",
-//   user: "remoteuser",
-//   password: "ff44Fourpm",
-//   database: "Golf",
-// });
 
 connection.connect();
 
